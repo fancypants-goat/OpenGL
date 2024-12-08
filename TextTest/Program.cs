667d5c06313123f5956523104555db7d098144c5
fancypants-goat <128519392+fancypants-goat@users.noreply.github.com>
2024-12-08 17:39:21 +0100

    stuff

--- TextTest/Program.cs
+++ TextTest/Program.cs
@@ -13,13 +13,8 @@ public class Game : Program
     {
         new Camera(new Vector3(0, 0, -10), new Vector3(0), 100, 0, 1000, true);
 
-        GameObject text = new GameObject("text");
-        text.AddComponent<UITransform>(new Vector2(0, 0), new Vector2(10, 10), ModelMode.Static);
-        text.AddComponent<Text>(Assets.GetFilePath("Roboto-Black.ttf"), "Hello", 32);
-
-        Matrix4 ortho = Matrix4.CreateOrthographicOffCenter(0, ClientSize.X, ClientSize.Y, 0, -1, 1);
-        Vector3 position = new(1, 5, 0);
-        Vector3 result = (ortho * new Vector4(position)).Xyz;
-        Console.WriteLine(result);
+        GameObject text = new("text");
+        text.AddComponent<UITransform>(new Vector2(0, 0), new Vector2(0, 0), ModelMode.Static);
+        text.AddComponent<Text>(Assets.GetFilePath("Roboto-Black.ttf"), "Hello, World!", 50);
     }
 }
