10f1d24cea7da208001f62da0fc4053ee5c23bbc
Fancypants <edingmichiel1@gmail.com>
2024-12-08 16:48:34 +0100

    Update Program.cs

--- TextTest/Program.cs
+++ TextTest/Program.cs
@@ -13,8 +13,6 @@ public class Game : Program
     {
         new Camera(new Vector3(0, 0, -10), new Vector3(0), 100, 0, 1000, true);
 
-        Graphics.BackColor(110, 61, 22);
-
         GameObject text = new GameObject("text");
         text.AddComponent<UITransform>(new Vector2(0, 0), new Vector2(10, 10), ModelMode.Static);
         text.AddComponent<Text>(Assets.GetFilePath("Roboto-Black.ttf"), "Hello", 32);
